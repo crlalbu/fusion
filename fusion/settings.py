1a21173a23abce8d8adc7c3da257ae2a0233b89f
Carlos Cavalcanti <crl.albu@gmail.com>
2021-02-08 08:50:56 -0300

    admin lte

--- fusion/settings.py
+++ fusion/settings.py
@@ -32,6 +32,11 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
+
+    'django_adminlte',
+    'django_adminlte_theme',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'core',
+    
 ]
 
 MIDDLEWARE = [
